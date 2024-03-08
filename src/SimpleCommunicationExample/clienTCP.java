fc96f13d59d9d6b451253d88415d2a368b2fb4af
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-03-08 02:08:53 +0200

    Simple Communication

---
+++ src/SimpleCommunicationExample/clienTCP.java
@@ -0,0 +1,29 @@
+package SimpleCommunicationExample;
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.util.*;
+public class clienTCP {
+    public static void main(String[] args) throws Exception {
+        try {
+            Socket socket= new Socket("127.0.0.1",6000);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());//!IF Server is not running will throw ex//
+            System.out.println("Enter Message: ");
+            Scanner scanner =new Scanner(System.in);
+            String message = "";
+            
+            do {
+                message = scanner.nextLine();
+                dataOutputStream.writeUTF(message); 
+                dataOutputStream.flush();
+            } while (!message.equals("stop"));
+			scanner.close();
+            dataOutputStream.close();
+            socket.close();
+        
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
+			     //*getOutputStream() method returns an output stream associated with this socket, 
+                    //*which allows the program to send data to the other end of the connection. */
---
+++ src/SimpleCommunicationExample/serverTCP.java
@@ -0,0 +1,30 @@
+package SimpleCommunicationExample;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class serverTCP {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket =new ServerSocket(6000);
+            System.out.println("Server is Running, waiting for client...");
+            Socket socket = serverSocket.accept();//*Block waiting client Request*/
+            System.out.println("Accepts incoming client connections");
+
+            DataInputStream dataInputStream =new DataInputStream(socket.getInputStream());
+            String message ="";
+            while (!message.equals("stop")) {
+                message = dataInputStream.readUTF();
+                System.out.println("New Message: "+ message);
+            }
+            System.out.println("Server Closing");
+            dataInputStream.close();
+            socket.close();
+            serverSocket.close();
+            
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        
+    }
+}
